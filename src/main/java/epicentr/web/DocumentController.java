47607f0f2ac4cedfe3a9202b2f408f2ba3746228
Andriy23780 <Andriy23780@gmail.com>
2020-07-16 18:26:32 +0300

    file upload to server

--- src/main/java/epicentr/web/DocumentController.java
+++ src/main/java/epicentr/web/DocumentController.java
@@ -2,6 +2,7 @@ package epicentr.web;
 
 import epicentr.entities.Message;
 import epicentr.repositories.RoleRepository;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.ServletContext;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.UUID;
+
 /**
  * @author Ramesh Fadatare
  *
@@ -22,6 +29,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class DocumentController
 {
+    @Autowired
+    ServletContext context;
 
     @Autowired
     private UserRepository userRepository;
@@ -39,9 +48,38 @@ public class DocumentController
     }
 
     @PostMapping("/document")
-    public String saveDocument(@RequestParam("images[]") MultipartFile[] images,
+    public String saveDocument(@RequestParam("images[]") MultipartFile[] files,
                                RedirectAttributes redirectAttributes)
     {
+        for (int i = 0; i < files.length; i++) {
+            MultipartFile file = files[i];
+
+            String name = UUID.randomUUID().toString()+"." + FilenameUtils.getExtension(file.getOriginalFilename());;
+            try {
+                byte[] bytes = file.getBytes();
+
+                // Creating the directory to store file
+                String rootPath =  context.getRealPath("resources/");
+                System.out.println("---------"+rootPath);
+                File dir = new File(rootPath + File.separator + "uploads");
+                if (!dir.exists())
+                    dir.mkdirs();
+
+                // Create the file on server
+                File serverFile = new File(dir.getAbsolutePath()
+                        + File.separator + name);
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+//                logger.info("Server File Location="
+//                        + serverFile.getAbsolutePath());
+
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        }
         return "redirect:/home";
     }
+
 }
