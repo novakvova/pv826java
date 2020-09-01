5cd60022cc05a42202eb3a45bf608a550f9e2dff
Volodymyr Novak <novakvova@gmail.com>
2020-09-01 19:35:27 +0300

    Add native app navigation

--- native/App.js
+++ native/App.js
@@ -7,7 +7,12 @@
  */
 
 import React, {Component} from 'react';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 import HomeScrean from './components/Home/HomeScrean';
+import {DrawerContent} from "./components/CustomElements/DrawerContent";
 import {
   SafeAreaView,
   StyleSheet,
@@ -24,6 +29,29 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+const MainNavigator = createStackNavigator(
+  {
+    Home: {screen: HomeScrean},
+ 
+  },
+  {
+    initialRouteName: 'Home',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#777777',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  },
+);
+
+const Drawer = createDrawerNavigator();
+const RootContainer = createAppContainer(MainNavigator);
+
 
 class App extends React.Component {
   render() {
