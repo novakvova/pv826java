126f64fc7b87b68f4d4fe383914bcf612d683db0
Volodymyr Novak <novakvova@gmail.com>
2020-09-02 20:16:53 +0300

    Add category relationships to product

--- src/main/java/epicentr/repositories/ProductRespository.java
+++
@@ -1,8 +0,0 @@
-package epicentr.repositories;
-
-
-import epicentr.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRespository extends JpaRepository<Product, Long> {
-}
---
+++ src/main/java/pv826/entities/Category.java
@@ -0,0 +1,56 @@
+package pv826.entities;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products;
+
+    public Category() {
+        products = new ArrayList<Product>();
+    }
+
+    public void addProduct(Product product) {
+        product.setCategory(this);
+        products.add(product);
+    }
+
+    public void removeProduct(Product product){
+        products.remove(product);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
