9a76f1eac9cebfbdc0c307463d2f9f921b53d3e9
Volodymyr Novak <novakvova@gmail.com>
2020-09-08 19:03:38 +0300

    Update login and register form

--- src/main/java/pv826/config/WebSecurityConfig.java
+++ src/main/java/pv826/config/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/home")
+                .defaultSuccessUrl("/")
                 .failureUrl("/login?error")
                 .permitAll()
                 .and()
