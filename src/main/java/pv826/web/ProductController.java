e2da7248a05f0a12eba7cf3d6f3d33100f26a018
Karonuk <losha.pisnyak42@gmail.com>
2020-09-17 17:42:09 +0300

    initial commit

--- src/main/java/pv826/web/ProductController.java
+++ src/main/java/pv826/web/ProductController.java
@@ -1,13 +1,58 @@
 package pv826.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoProperties;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import pv826.entities.Category;
+import pv826.entities.Product;
+import pv826.repositories.CategoryRepository;
+import pv826.repositories.ProductRespository;
+import pv826.services.StorageService;
+
+import javax.servlet.ServletContext;
 
 @Controller
 public class ProductController {
-    @GetMapping("/products")
-    public String list(){
-        return "products";
+    @Autowired
+    ServletContext context;
+    private final StorageService storageService;
+
+
+    @Autowired
+    private ProductRespository productRespository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public ProductController(StorageService storageService) {
+        this.storageService = storageService;
     }
+
+
+
+    @GetMapping("/product")
+    public String home(Model model){
+        model.addAttribute("products", productRespository.findAll());
+        return ("/product/index");
+    }
+
+    @GetMapping("/product/create")
+    public String getCategories(Model model){
+        model.addAttribute("categories",categoryRepository.findAll());
+        return("/product/create");
+    }
+    @PostMapping("/product/create")
+    public String create(Product productModel, Model model){
+        Product newProduct=new Product();
+        newProduct.setName(productModel.getName());
+        newProduct.setPrice(productModel.getPrice());
+        newProduct.setDescription(productModel.getDescription());
+        productRespository.save(newProduct);
+        return("redirect:/product");
+    }
+
 }
 
